17cdfee994dddcf33b6d96a34014d2a1886bea3a
Hanna Munoz <hannamunoz@gmail.com>
2021-02-19 18:40:15 -0500

    updating generated README format

--- README.md
+++ README.md
@@ -1,36 +1,55 @@
-# My Project
-    [![GitHub License](undefined)](undefined)
-    
-    ## *Table of Contents*
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Author](#author)
-    - [Questions](#questions)
-    
-    ## *Description*
-    My Project Description
-    
-    ## *Installation*
-    npm i inquirer
-    
-    ## *Usage*
-    Answer prompted questions to generate ReadME
-    
-    ## *Contributing*
-    undefined
-    
-    ## *Tests*
-    
+# README Generator
+[![GitHub License](https://img.shields.io/badge/License-Apache--2.0-yellow)](https://opensource.org/licenses/Apache-2.0)
 
-    ## *Author*
-    Hanna Munoz
-    
-    ## *Questions*
-    - GitHub Username: **[hannamunoz]
-    - Email: **[hannamunoz@gmail.com]
-    - LinkedIn: **[https://www.linkedin.com/in/hanna-munoz-179993b2/]
-    
-    This project is MIT licensed
+---------------
+
+## *Table of Contents*
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Author](#author)
+- [Questions](#questions)
+
+---------------
+
+## *Description*
+This project allows you to create a polished README by using the terminal to answer a few questions.
+
+---------------
+
+## *Installation*
+npm i inquirer
+
+---------------
+
+## *Usage*
+Answer prompted questions to generate ReadME
+
+---------------
+
+## *Contributing*
+Please use a new branch before contributing
+
+---------------
+
+## *Tests*
+
+
+---------------
+
+## *Author*
+Hanna Munoz
+
+---------------
+
+## *Questions*
+Please feel to reach me at any of the points of contact below:
+- GitHub: github.com/hannamunoz
+- Email: [hannamunoz@gmail.com]
+- LinkedIn: https://www.linkedin.com/in/hanna-munoz-179993b2/
+
+---------------
+
+This project is Apache licensed
--- index.js
+++ index.js
@@ -95,68 +95,88 @@ inquirer.prompt([
     }
 ]).then((answers) => {
     const { title, description, installation, usage, license, contributors, 
-        contributing, tests, username, contact, linkedIn } = answers;
+        contribution, tests, username, contact, linkedIn } = answers;
     let licenseBadge;
     let licenseUrl;
-
-    switch (license) {
+console.log(license)
+    switch (license[0]) {
         case 'MIT':
-            licenseBadge = 'https://img.shields.io/badge/License-MIT-blue'
+            licenseBadge = 'https://img.shields.io/badge/License-MIT-blue';
             licenseUrl = 'https://opensource.org/licenses/MIT';
             break;
         case 'GNU':
-            licenseBadge = 'https://img.shields.io/badge/License-GNU-green'
+            licenseBadge = 'https://img.shields.io/badge/License-GNU-green';
             licenseUrl = 'https://opensource.org/licenses/gpl-license';
             break;
         case 'Apache':
-            licenseBadge = 'https://img.shields.io/badge/License-Apache--2.0-yellow'
+            licenseBadge = 'https://img.shields.io/badge/License-Apache--2.0-yellow';
             licenseUrl = 'https://opensource.org/licenses/Apache-2.0';
             break;
         case 'ISC':
-            licenseBadge = 'https://img.shields.io/badge/License-ISC-orange'
+            licenseBadge = 'https://img.shields.io/badge/License-ISC-orange';
             licenseUrl = 'https://opensource.org/licenses/ISC';
             break;
         case 'Open':
-            licenseBadge = 'https://img.shields.io/badge/License-Open-red'
+            licenseBadge = 'https://img.shields.io/badge/License-Open-red';
             licenseUrl = 'https://opensource.org/licenses/OSL-3.0';
     }
 
-    const ReadMeFile = `# ${title}
-    [![GitHub License](${licenseBadge})](${licenseUrl})
-    
-    ## *Table of Contents*
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Author](#author)
-    - [Questions](#questions)
-    
-    ## *Description*
-    ${description}
-    
-    ## *Installation*
-    ${installation}
-    
-    ## *Usage*
-    ${usage}
-    
-    ## *Contributing*
-    ${contributing}
-    
-    ## *Tests*
-    ${tests}
+    const ReadMeFile =
+`# ${title}
+[![GitHub License](${licenseBadge})](${licenseUrl})
 
-    ## *Author*
-    ${contributors}
-    
-    ## *Questions*
-    - GitHub Username: **[${username}]
-    - Email: **[${contact}]
-    - LinkedIn: **[${linkedIn}]
-    
-    This project is ${license} licensed`;
+---------------
+
+## *Table of Contents*
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Author](#author)
+- [Questions](#questions)
+
+---------------
+
+## *Description*
+${description}
+
+---------------
+
+## *Installation*
+${installation}
+
+---------------
+
+## *Usage*
+${usage}
+
+---------------
+
+## *Contributing*
+${contribution}
+
+---------------
+
+## *Tests*
+${tests}
+
+---------------
+
+## *Author*
+${contributors}
+
+---------------
+
+## *Questions*
+Please feel to reach me at any of the points of contact below:
+- GitHub: github.com/${username}
+- Email: [${contact}]
+- LinkedIn: ${linkedIn}
+
+---------------
+
+This project is ${license} licensed`;
 
     fs.writeFile("README.md", ReadMeFile, err => {
         if (err) {
